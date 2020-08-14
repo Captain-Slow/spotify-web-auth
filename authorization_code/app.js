37fa2291fe3b03836bd82a1a8761fa5ac1097d69
captainslow <jjkew9198@gmail.com>
2020-08-14 17:13:50 +0800

    change to use env variables

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -17,9 +17,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-var client_id = ""; // Your client id
-var client_secret = ""; // Your secret
-var redirect_uri = ""; // Your redirect uri
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = process.env.REDIRECT_URL; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
